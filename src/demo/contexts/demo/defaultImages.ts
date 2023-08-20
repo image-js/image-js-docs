bc5599130b02ef2c043e45d7a34997c21e055214
Michaël Zasso <targos@protonmail.com>
2023-08-20 17:45:14 +0200

    refactor: move standard images to fixed URLs

--- src/demo/contexts/demo/defaultImages.ts
+++ src/demo/contexts/demo/defaultImages.ts
@@ -1,10 +1,12 @@
 import { UrlOption } from '../importImage/importImageContext';
 
+const standardUrl = 'https://image-js.github.io/image-dataset-demo/standard';
+
 export const defaultImages: UrlOption[] = [
-  { type: 'url', value: '/img/standard/Lenna.png', label: 'Lenna' },
-  { type: 'url', value: '/img/standard/barbara.jpg', label: 'Barbara' },
-  { type: 'url', value: '/img/standard/boat.png', label: 'Standard boat' },
-  { type: 'url', value: '/img/standard/mandrill.png', label: 'Mandrill' },
-  { type: 'url', value: '/img/standard/peppers.png', label: 'Peppers' },
-  { type: 'url', value: '/img/standard/house.png', label: 'House' },
+  { type: 'url', value: `${standardUrl}/lenna.png`, label: 'Lenna' },
+  { type: 'url', value: `${standardUrl}/barbara.jpg`, label: 'Barbara' },
+  { type: 'url', value: `${standardUrl}/boat.png`, label: 'Standard boat' },
+  { type: 'url', value: `${standardUrl}/mandrill.png`, label: 'Mandrill' },
+  { type: 'url', value: `${standardUrl}/peppers.png`, label: 'Peppers' },
+  { type: 'url', value: `${standardUrl}/house.png`, label: 'House' },
 ];
