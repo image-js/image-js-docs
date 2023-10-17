2fb57bc5212cc8b7b562b75b3bebaeeffe820ced
Daniel Kostro <kostro.d@gmail.com>
2023-10-17 14:46:33 +0200

    fix: Image with BINARY color model is unreachable

--- src/demo/utils/image.ts
+++ src/demo/utils/image.ts
@@ -2,8 +2,9 @@ import { Image } from 'image-js';
 
 export function imageToMask(image: Image) {
   switch (image.colorModel) {
-    case 'GREY':
     case 'BINARY':
+      throw new Error('BINARY Image does not exist in image-js');
+    case 'GREY':
       return image.threshold();
     case 'GREYA':
       return image.convertColor('GREY').threshold();
