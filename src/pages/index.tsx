a602fa9d016aee5bc55aa4cbded47a15b50e0fdb
EscapedGibbon <maximgolubev2@gmail.com>
2024-06-19 12:55:04 +0200

    docs: add a text to the front page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import React from 'react';
@@ -14,12 +13,24 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p>
+          ImageJS is a JavaScript library designed to facilitate image
+          processing and manipulation.ImageJS provides a wide range of features
+          for developers to enhance, analyze, and transform images seamlessly.
+          It includes functions for common tasks such as resizing, cropping,
+          filtering, and color adjustments, all performed client-side without
+          the need for server-side processing. This makes it an ideal tool for
+          web applications that require real-time image editing capabilities.
+          ImageJS is optimized for performance, ensuring quick and efficient
+          operations even with high-resolution images, and is easy to integrate
+          into existing projects with its intuitive API.
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/Getting started"
           >
-            Docusaurus Tutorial - 5min ⏱️
+            Let&#39;s get started.
           </Link>
         </div>
       </div>
@@ -31,12 +42,11 @@ export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Welcome to ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <HomepageHeader />
       </main>
     </Layout>
   );
