8fc1ec3de243ebd7c3757a27d7ecc9a7ebecdc4d
Daniel Kostro <kostro.d@gmail.com>
2025-08-04 11:53:56 +0200

    chore: fix type issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ function HomepageHeader() {
   );
 }
 
-export default function Home(): JSX.Element {
+export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
