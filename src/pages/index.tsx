f2e388f268b4d30190962bfbea2b82c4a8a8179c
Michaël Zasso <targos@protonmail.com>
2025-08-20 10:44:46 +0200

    refactor: rework and simplify Getting started page (#142)
    - Put system requirements at the top
    - Change slug to `getting-started`
    - Change to MDX since it uses React components.
    - Remove Vite tutorial
    - Proofread

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="docs/Getting started"
+            to="docs/getting-started"
           >
             Let&apos;s get started
           </Link>
